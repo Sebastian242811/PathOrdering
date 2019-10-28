03ef94c90b2e96bdc9bbc94216c33a152b9be092
Sebastian <hernandesebastian0@gmail.com>
2019-10-28 14:05:30 -0500

    Correccion model usuario

--- src/main/java/pe/com/pathOrder/model/Usuario.java
+++ src/main/java/pe/com/pathOrder/model/Usuario.java
@@ -12,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
 import pe.com.pathOrder.model.Authority;
 
